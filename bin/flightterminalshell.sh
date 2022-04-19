ba9a063ff61eb2e763b27323f366135bf8ec9096
Gary Sangwell <gary.sangwell@alces-software.com>
2022-04-19 16:52:15 +0000

    Disable pubkey auth for flight terminal login.

--- bin/flightterminalshell.sh
+++ bin/flightterminalshell.sh
@@ -7,4 +7,4 @@ while true ; do
 	if [ $valid_user -eq 0 ] ; then break ; fi ;
 done;
 
-ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $username@appliance02
+ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o PubkeyAuthentication=no $username@appliance02
