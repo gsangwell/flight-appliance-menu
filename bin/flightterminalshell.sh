5114ac7a46ea1c047370da64700279d536d3947e
Gary Sangwell <gary.sangwell@alces-software.com>
2022-04-19 10:34:16 +0000

    Add script for running cli via flight terminal.

---
+++ bin/flightterminalshell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while true ; do
+	read -p 'Username: ' username
+	valid_user=$(id -u "$username" >/dev/null; echo $?)
+
+	if [ $valid_user -eq 0 ] ; then break ; fi ;
+done;
+
+ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $username@appliance02
