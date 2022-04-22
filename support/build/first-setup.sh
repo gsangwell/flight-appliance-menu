00b52619049f755af670b52d59622a5e3a30902d
Gary Sangwell <gary.sangwell@alces-software.com>
2022-04-22 16:22:28 +0100

    Check Selinux is disabled + ssh passwords enabled.

--- support/build/first-setup.sh
+++ support/build/first-setup.sh
@@ -3,6 +3,20 @@
 echo -e "\nAlces Hub - Setup:"
 echo -e "-------------------"
 
+# Check selinux disabled & ssh password enabled
+selin=$(sudo getenforce)
+sshpass=$(ssh -n -o Batchmode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null DOES_NOT_EXIST@localhost 2>&1 | grep -c password)
+
+if [[ "$selin" != "Disabled" ]] ; then
+    echo -e "Please disable Selinux, reboot and re-run this setup."
+    exit 1
+fi
+
+if [ $sshpass -eq 0 ] ; then
+    echo -e "Please enable SSH password logins and re-run this setup."
+    exit 1
+fi
+
 # Config
 echo -n "Enter appliance hostname: "; read APPL_NAME
 echo -n "Enter Alces Support Username: "; read VPN_USERNAME
@@ -12,6 +26,10 @@ echo "$VPN_PASSWORD" | sed 's/./*/g'
 # Configure appliance
 sudo sed -i "s/appliance_name:/appliance_name: ${APPL_NAME}/g" /opt/appliance/cfg/config.yaml
 sudo hostnamectl set-hostname "$APPL_NAME"
+
+sudo bash -c 'cat >> /etc/hosts' << EOF
+127.0.0.1    ${APPL_NAME}
+EOF
 
 # Configure vpn
 sudo bash -c 'cat > /etc/openvpn/client/auth.alces-support' << EOF
